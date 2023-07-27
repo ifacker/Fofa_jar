7c67f3edbf3b7a6fc9e809361212b186a0010ceb
ifacker <2677863724@qq.com>
2023-07-27 10:44:39 +0800

    update

---
+++ src/main/java/plugins/PluginDemo/Main.java
@@ -0,0 +1,48 @@
+package plugins.PluginDemo;
+
+import Internet.ProxyInternet;
+import Plugin.Plugin;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.net.Proxy;
+
+
+public class Main implements Plugin {
+    @Override
+    public String getName(){
+        return "demo插件";
+    }
+    @Override
+    public Node getContent(){
+        // 创建插件的内容
+        VBox content = new VBox();
+        Label label = new Label("demo");
+        content.getChildren().add(label);
+
+        OkHttpClient client = new ProxyInternet().newClient();
+
+        // 创建 HTTP 请求
+        Request request = new Request.Builder()
+                .url("https://www.baidu.com")
+                .build();
+
+        Response response = null;
+        try {
+            // 发送请求
+            response = client.newCall(request).execute();
+            // 处理响应
+            System.out.println(response.body().string());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return  content;
+    }
+}
